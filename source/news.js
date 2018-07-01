251f34a8281e2b158370e358f5d5ceb4a853d158
Laura Petre <laura.d.petre@gmail.com>
2018-07-01 22:26:04 +0100

    Created controller , added API keys

--- source/news.js
+++ source/news.js
@@ -1,11 +1,10 @@
 var NEWSMODULE= (function(exports) {
-  exports.News = News;
   function News(title, url, summary, thumbnail) {
     this.title = title;
     this.url = url;
     this.summary = summary;
     this.thumbnail = thumbnail;
   };
-
+  exports.News = News;
   return exports;
 })(this);
